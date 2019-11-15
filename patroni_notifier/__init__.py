1e7ce94bdda8a34dd52501690b86e37a2c0ee3bd
Jared Vacanti <jaredvacanti@gmail.com>
2019-11-14 23:31:14 -0600

    parsing config file from default patroni.yml

--- patroni_notifier/__init__.py
+++ patroni_notifier/__init__.py
@@ -1,6 +1,4 @@
-from patroni_notifier.core import *
-
 __NAME__ = "Patroni Notifier"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __license__ = "MIT"
 __author__ = "Jared Vacanti <jaredvacanti@gmail.com>"
